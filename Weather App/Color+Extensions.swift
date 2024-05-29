8d35df6454be67e3613b64735f7d77ea12415318
Augusto Neves <augusto.agt1995@gmail.com>
2024-05-28 21:10:01 -0300

    creating components

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Augusto Enoque das Neves Gouveia on 28/05/24.
+//
+
+import Foundation
---
+++ Weather App/HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  Weather App
+//
+//  Created by Augusto Enoque das Neves Gouveia on 28/05/24.
+//
+
+import UIKit
+
+class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    
+}
